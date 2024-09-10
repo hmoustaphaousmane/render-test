d32d9b45558b1aeedc02aa825c4c0a1f80531bda
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-09-10 16:19:32 +0100

    feat: serving static files from the backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # render-test
+
+* App link is [here](https://render-test-56yd.onrender.com/)
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-// require('dotenv').config()
 
 const app = express()
 app.use(cors())
+app.use(express.static('dist'))
 
 app.use(express.json())
 
