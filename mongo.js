0ee1debc7bd7a981cf9fdb3d5f36ffb0b16ac573
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-09-13 12:21:12 +0100

    feat: notes unit tests

--- mongo.js
+++ mongo.js
@@ -1,18 +1,17 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-
-const uri =
-  `mongodb+srv://undjonline:${password}@fs-notes-cluster.360dr.mongodb.net/noteApp?retryWrites=true&w=majority&appName=fs-notes-cluster`
+// const uri = process.env.MONGODB_URI
+const testUri = process.env.TEST_MONGODB_URI
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(uri)
+// mongoose.connect(uri)
+mongoose.connect(testUri)
+  .then(result => logger.info('connected to MongoDB'))
+  .catch(error =>
+    logger.error('error connecting to MongoDB', error.message))
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -21,21 +20,31 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
+const note1 = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+// save a note1
+note1.save().then(result => {
+  console.log('note1 saved!')
+})
+
+const note2 = new Note({
   content: 'GET and POST are the most important methods of HTTP protocol',
   important: true,
 })
 
-// save a note
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-// get all notes using find method of the model Note
-Note.find({ important: true }).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+// save a note2
+note2.save().then(result => {
+  console.log('note2 saved!')
   mongoose.connection.close()
 })
+
+// get all notes using find method of the model Note
+// Note.find({ important: true }).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
---
+++ tests/note_api.test.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Note = require('../models/note')
+
+const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript'
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({}) // clear out the database
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test('notes are returned as json', async () => {
+  // console.log(process.env.NODE_ENV === 'test')
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('HTML is easy')) // is the argument truthy
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
