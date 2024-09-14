9c5e6c188b99395f9e0bbe1a09759d159df4f134
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-09-14 14:58:51 +0100

    feat: user administration

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,11 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -23,6 +26,8 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
@@ -30,10 +35,14 @@ notesRouter.post('/', async (request, response, next) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
   try {
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.status(201).json(savedNote)
   } catch (exception) {
     next(exception)
